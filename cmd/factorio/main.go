897aeec4df4c547e4941454c70101d3116937fd0
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2024-10-02 08:37:30 -0500

    add -version, -help, -- command line options

--- cmd/factorio/main.go
+++ cmd/factorio/main.go
@@ -1,20 +1,44 @@
 package main
 
 import (
+	"github.com/mcandre/factorio"
+
+	"flag"
+	"fmt"
 	"log"
 	"os"
-
-	"github.com/mcandre/factorio"
 )
 
-func main() {
-	if len(os.Args) == 0 {
-		log.Fatalf("error: missing program name\n")
+var flagVersion = flag.Bool("version", false, "show version")
+var flagHelp = flag.Bool("help", false, "show usage menu")
+
+func usage() {
+	program, err := os.Executable()
+
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
 	}
 
-	args := os.Args[1:]
+	fmt.Printf("Usage: %v [OPTION] [-- <go build flags>]\n", program)
+	flag.PrintDefaults()
+}
+
+func main() {
+	flag.Parse()
+
+	switch {
+	case *flagVersion:
+		fmt.Println(factorio.Version)
+		os.Exit(0)
+	case *flagHelp:
+		usage()
+		os.Exit(0)
+	}
+
+	args := flag.Args()
 
 	if err := factorio.Port(args); err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 }
